82e2e4be4ce356608d9f313a9d347e1a4c4a8cbd
yodamad <mvincent.it@gmail.com>
2015-11-02 21:50:57 +0100

    Add ID to masterdata model

--- src/main/java/fr/jahland/baby/model/Masterdata.java
+++ src/main/java/fr/jahland/baby/model/Masterdata.java
@@ -1,9 +1,13 @@
 package fr.jahland.baby.model;
 
+import org.springframework.data.annotation.Id;
+
 /**
  * Created by mvincent on 02/11/2015.
  */
 public class Masterdata {
+    @Id
+    public Integer id;
     public String label;
-    public MasterdataType type;
+    public String type;
 }
