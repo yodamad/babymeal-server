a298506910f9a9b3657ddfb834d95aedfc3a3c27
Mat V <mvincent.it@gmail.com>
2015-12-23 21:54:40 +0100

    Add meat in masterdata

--- src/main/java/fr/jahland/baby/model/MasterdataType.java
+++ src/main/java/fr/jahland/baby/model/MasterdataType.java
@@ -4,5 +4,5 @@ package fr.jahland.baby.model;
  * Created by mvincent on 01/11/2015.
  */
 public enum MasterdataType {
-    DRUG, FRUIT, GARNISH, MEALTYPE, MILKTYPE, VEGETABLE;
+    DRUG, FRUIT, GARNISH, MEALTYPE, MILKTYPE, VEGETABLE, MEAT;
 }
